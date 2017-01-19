289b188c3a302eaf858bc75256ffc272d863fc86
Prima <prima@skyshi.com>
2017-01-19 13:28:08 +0700

    Refactor middleware

--- server.js
+++ server.js
@@ -2,17 +2,8 @@ var express = require ('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private row hit!');
-		next();
-	},
-	logger: function (req, res, next) {
+var middleware = require('./middleware.js');
 
-		console.log('Request :' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
 
 app.use(middleware.logger);
 app.get('/about', middleware.requireAuthentication, function (req, res) {
